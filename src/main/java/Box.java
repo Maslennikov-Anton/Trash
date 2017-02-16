a143c885479941a9bc8461f9a5f5f4bcbea6e5d4
Aomine <Antojkoo@yandex.ru>
2017-02-16 15:28:04 +0300

    generics

---
+++ src/main/java/Box.java
@@ -0,0 +1,19 @@
+
+
+
+public class Box<T> {
+
+    // BEGIN (write your solution here)
+    final T value;
+
+    public Box(final T value) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    // END
+
+}
