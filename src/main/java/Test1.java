e11730f4e34ab087f2233af7a79587fa3cf7d304
Aomine <Antojkoo@yandex.ru>
2017-02-16 17:01:34 +0300

    extends generic

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -4,10 +4,10 @@ import java.util.Map;
 public class Test1 {
     public static void main(String[] args) {
 
-        Map<String,String> a = new HashMap<>();
-        System.out.println(a.put(null,null));
-        System.out.println(a.put("a","a"));
-        System.out.println(a.put("a","b"));
+//        Map<String,String> a = new HashMap<>();
+//        System.out.println(a.put(null,null));
+//        System.out.println(a.put("a","a"));
+//        System.out.println(a.put("a","b"));
 
 
         //System.out.println(a.size());
---
+++ src/main/java/Tuple.java
@@ -0,0 +1,25 @@
+
+
+
+public class Tuple<T1, T2> {
+
+    // BEGIN (write your solution here)
+    private final T1 left;
+    private final T2 right;
+
+    public Tuple(T1 left, T2 right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public T1 getLeft() {
+        return left;
+    }
+
+    public T2 getRight() {
+        return right;
+    }
+
+    // END
+
+}
